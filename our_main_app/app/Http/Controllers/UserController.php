7462129c9336b14e8bd311e28c1ee3f47fbc9ce5
mohmmad <azimi.jozani.mohammad@gmail.com>
2023-02-17 07:59:35 +0330

    add error message from form validation in template

--- our_main_app/app/Http/Controllers/UserController.php
+++ our_main_app/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Validation\Rule;
 
 
 class UserController extends Controller
@@ -12,10 +13,16 @@ class UserController extends Controller
     public function register(Request $request)
     {
         $incoming_fields = $request->validate([
-            'username'=> 'required',
-            'email'=> 'required',
-            'password'=> 'required',
+            'username'=> ['required', 'min:3', 'max:20', Rule::unique(
+                'users', 'username'
+            )],
+            'email'=> ['required', 'email', Rule::unique(
+                'users', 'email')
+            ],
+            'password'=> ['required', 'min:8', 'confirmed'],
         ]);
+
+        $incoming_fields['password'] = bcrypt($incoming_fields['password']);
         User::create($incoming_fields);
         return 'hello register';
 
